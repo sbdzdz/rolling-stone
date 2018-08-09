ee281e721f57b3837e29b6f18eaa3b7eaa4f48ff
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2018-08-09 22:43:08 +0200

    Test the API

--- download_lyrics.py
+++ download_lyrics.py
@@ -6,4 +6,6 @@ config.read('config.ini')
 token = config['GENIUS']['ACCESS_TOKEN']
 
 api = genius.Genius(token)
-artist = api.search_artist('Bob Dylan', max_songs=3)
+artist = api.search_artist('Bob Dylan', max_songs=10)
+for song in artist.songs:
+    print(song.title, song.year)
