09f0a9c735e8d20ead01cf1193aed3a3ba64247f
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2018-08-26 21:46:56 +0200

    Remove song limit

--- download_lyrics.py
+++ download_lyrics.py
@@ -10,7 +10,6 @@ config.read('config.ini')
 token = config['GENIUS']['ACCESS_TOKEN']
 
 api = genius.Genius(token)
-bob = api.search_artist('Bob Dylan', max_songs=20)
+bob = api.search_artist('Bob Dylan')
 bob._songs = [s for s in bob.songs if written_by(bob.name, s)]
-
 bob.save_lyrics(filename='data/dylan')
